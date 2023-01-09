0cf29191e9a98dbd1ae4e7d02d4a5370c2bd3050
IvanSanczewski <isanczewski@gmail.com>
2023-01-09 12:55:28 +0200

    Add pages

---
+++ src/pages/Movies.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="movies">
+        <h1>This is the Movies page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
