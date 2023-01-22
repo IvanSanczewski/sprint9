b47a48a8db260b7fc0ca5b55339b60ecd4dd2444
IvanSanczewski <isanczewski@gmail.com>
2023-01-22 14:44:59 +0200

    Set user,  borrow & return date to book. DOES NOT POST INTO JSON

--- src/components/BookDetails.vue
+++ src/components/BookDetails.vue
@@ -8,7 +8,8 @@
             <p>Language: {{ storeGetBooks.editionDetails.language }}</p>
             <p>ISBN: {{ storeGetBooks.editionDetails.isbn }}</p>
             <!-- <p>Available: {{ storeGetBooks.editionDetails.available }}</p> -->
-            <button v-if="storeGetBooks.editionDetails.available">Borrow this item</button>
+
+            <button v-if="storeGetBookAvailability.bookIsAvailable" @click="storeGetBookAvailability.borrowToUser(storeGetUser.user.email, storeGetBooks.editionDetails)">Borrow this item</button>
             <button v-else>Reserve this item</button>
             <div class="extras">
                 <button>Check borrow policy</button>
@@ -21,13 +22,24 @@
 
 <script setup>
 import { useGetBooksStore } from '../stores/getBooks'
+import { useGetUserStore } from '../stores/getUser'
+import { useGetBookAvailability } from '../stores/getBookAvailability'
+
 // import Books from '../pages/Books.vue'
 
 
 const storeGetBooks = useGetBooksStore()
+const storeGetUser = useGetUserStore()
+const storeGetBookAvailability = useGetBookAvailability()
 
+console.log(storeGetBooks.editionDetails.isbn)
+console.log(storeGetUser.user.email)
+console.log(storeGetBooks.editionDetails)
 
-// TOGGLE BETWEEN DISPLAYBOOK SO IT IS SHOWED ONLY WHEN TRUE
+// storeGetBookAvailability.isAvailable(storeGetBooks.editionDetails.isbn)
+storeGetBookAvailability.isAvailable(storeGetBooks.editionDetails.isbn)
+
+// TODO: TOGGLE BETWEEN DISPLAYBOOK SO IT IS SHOWED ONLY WHEN TRUE
 // const toggleDisplayBook = ((storeGetBooks) => {
 //     console.log('toggle active')
 //     return storeGetBooks.displayBook = true
--- src/stores/getBookAvailability.js
+++ src/stores/getBookAvailability.js
@@ -1,15 +1,15 @@
 import { defineStore } from 'pinia'
-import axios from 'axios'
-
+// import axios from 'axios'    
 
 const borrowedBooks_db_URL = 'http://localhost:3000/borrowedBooks'
 
 export const useGetBookAvailability = defineStore('getBookAvailability', {
     state: () => ({
-        borrowedBooks: {}
+        borrowedBooks: [],
+        newBorrowedBook: {},
+        bookIsAvailable: null
     }),
     getters: {
-
     },
     actions: {
         getBorrowedBooks() {
@@ -18,13 +18,59 @@ export const useGetBookAvailability = defineStore('getBookAvailability', {
                 .then (data => this.borrowedBooks = data)
         },
 
+        isAvailable(isbn) {
+            if (this.borrowedBooks.some(book => book.isbn === isbn)) {
+                console.log('this book is borrowed')
+                this.bookIsAvailable = false
+            } else {
+                this.bookIsAvailable = true
+                console.log('this book is available')
+            }
+        },
 
-        //     .then(data => this.borrowedBooks = data)
-        // console.log(this.borrowedBooks)
+        // asign borrowing user to book 
+        borrowToUser(user, book) {
+            book.available = false
+            let borrowDate = new Date
+            let initialDate = new Date // this date is used to calculate the 4 weeks borrow period, but when setDate method is applied, it sets the new date also to initialDate, therefore we do not use borrowDate to make the calculation
+            let returnDate = new Date(initialDate.setDate(initialDate.getDate() + 28))
+            
+            let borrow = {
+                user,
+                borrowDate,
+                returnDate
+            }
+            this.newBorrowedBook = { ...book, borrow}
 
-        // TODO: borrowToUser(email)       
-        // async borrowToUser(email) {
+            // this.newBorrowedBook = Object.assign(book,
+            //     {borrow: {
+            //         user,
+            //         borrowDate: new Date
+            // }})
+        
+            // this.addBorrowedBook(this.newBorrowedBook)
+
+            fetch(`https//localhost:3000/borrowedBooks`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(this.newBorrowedBook)
+            })
+        },
+        
+
+
+
+
+        // addBorrowedBook(book) {
+        //     console.log(book)
+
+        //     fetch('http://localhost:3000/borrowedBooks', {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/json' },
+        //         body: JSON.stringify(book)
+        //     })
 
         // }
+
     }
 })
