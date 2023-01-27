3d89e7383471a4e930238255754d9590a3a585de
IvanSanczewski <isanczewski@gmail.com>
2023-01-27 13:09:47 +0200

    Fixed footer on bottom of site

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
-  <!-- H E A D E R -->
-  <header>
-    <div class="container-header">
-      
-      <div class="sitcky-menu">
 
-        <!-- SOCIAL ICONS -->
-        <div class="social-icons">
-          <a href="#" class="social-fb"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
-          <a href="#" class="social-tw"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
-          <a href="#" class="social-ytb"><font-awesome-icon :icon="['fab', 'youtube']" /></a>
-        </div>
-        <!-- NATLIB -->
-        <div class="natlib">
-          <span class="title">the logo</span>
-          <span class="title">the NATLIB</span>
-        </div>
-        <!-- USER -->
-        <div class="user-actions">
-          <div class="user-log">
-            <!-- <span class="user-action">LogIn</span> -->
-            <!-- <div v-show="!storeGetUser.isLogged" @click="storeGetUser.getRegisteredUser" class="user-action">LogIn</div> --> <!-- DELETE?? -->
-        
-            <a href="#" v-show="!storeGetUser.isLogged" @click="storeGetUser.toggleDisplayLogIn" class="user-action">LogIn</a>
-            <a href="#" v-show="storeGetUser.isLogged" @click="storeGetUser.logOutUser" class="user-action">LogOut</a>
-            <LogIn v-if="storeGetUser.displayLogIn"/>
-            <!-- <div v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
+    <!-- H E A D E R -->
+    <header>
+      <div class="container-header">
+    
+        <div class="sitcky-menu">
+          <!-- SOCIAL ICONS -->
+          <div class="social-icons">
+            <a href="#" class="social-fb"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
+            <a href="#" class="social-tw"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+            <a href="#" class="social-ytb"><font-awesome-icon :icon="['fab', 'youtube']" /></a>
           </div>
-        
-          <div class="user-sign">
-            <a href="#" @click="storeGetUser.toggleDisplaySignIn" class="user-action">SignIn</a>
-            <SignIn v-if="storeGetUser.displaySignIn"/>
+          <!-- NATLIB -->
+          <div class="natlib">
+            <span class="title">the logo</span>
+            <span class="title">the NATLIB</span>
+          </div>
+          <!-- USER -->
+          <div class="user-actions">
+            <div class="user-log">
+              <!-- <span class="user-action">LogIn</span> -->
+              <!-- <div v-show="!storeGetUser.isLogged" @click="storeGetUser.getRegisteredUser" class="user-action">LogIn</div> --> <!-- DELETE?? -->
+    
+              <a href="#" v-show="!storeGetUser.isLogged" @click="storeGetUser.toggleDisplayLogIn" class="user-action">LogIn</a>
+              <a href="#" v-show="storeGetUser.isLogged" @click="storeGetUser.logOutUser" class="user-action">LogOut</a>
+              <LogIn v-if="storeGetUser.displayLogIn"/>
+              <!-- <div v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
+            </div>
+    
+            <div class="user-sign">
+              <a href="#" @click="storeGetUser.toggleDisplaySignIn" class="user-action">SignIn</a>
+              <SignIn v-if="storeGetUser.displaySignIn"/>
+            </div>
           </div>
         </div>
+        <div class="active-user" v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div>
+    
+    
+        <div>
+          <Navbar />
+          <!-- <div class="active-user" v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
+        </div>
+    
       </div>
+    </header>
+    
+    <!-- C O N T E N T -->
+    <main>
+      <RouterView />
+    </main>
+    <!-- F O O T E R -->
+    <footer>
+      <Footer />
+    </footer>
 
-      <div class="active-user" v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div>
-      
-      
-      <div>
-        <Navbar />
-        <!-- <div class="active-user" v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
-      </div>
-      
-    </div>
-  </header>
-  
-  <!-- C O N T E N T -->
-  <main>
-    <RouterView />
-  </main>
-
-  <!-- F O O T E R -->
-  <footer>
-    <Footer />
-  </footer>
 </template>
 
 <script setup>
