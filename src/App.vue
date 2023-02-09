be8cf0665309ee2f270a1871bce3a56c0735ee16
IvanSanczewski <isanczewski@gmail.com>
2023-02-09 13:25:20 +0200

    Fixed sticky menu width. MAKE IT STICKY

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,30 @@
     
         <div class="sitcky-menu">
           <!-- SOCIAL ICONS -->
-          <div class="social-icons">
-            <a href="#" class="social-fb"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
-            <a href="#" class="social-tw"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
-            <a href="#" class="social-ytb"><font-awesome-icon :icon="['fab', 'youtube']" /></a>
-          </div>
-          <!-- NATLIB -->
-          <div class="natlib">
-            <span class="title">the logo</span>
-            <span class="title">the NATLIB</span>
-          </div>
-          <!-- USER -->
-          <div class="user-actions">
-            <div class="user-log">
-              <!-- <span class="user-action">LogIn</span> -->
-              <!-- <div v-show="!storeGetUser.isLogged" @click="storeGetUser.getRegisteredUser" class="user-action">LogIn</div> --> <!-- DELETE?? -->
-    
-              <a href="#" v-show="!storeGetUser.isLogged" @click="storeGetUser.toggleDisplayLogIn" class="user-action">LogIn</a>
-              <a href="#" v-show="storeGetUser.isLogged" @click="storeGetUser.logOutUser" class="user-action">LogOut</a>
-              <LogIn v-if="storeGetUser.displayLogIn"/>
-              <!-- <div v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
+          <div class="sitcky-menu-content">
+            <div class="social-icons">
+              <a href="#" class="social-fb"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
+              <a href="#" class="social-tw"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+              <a href="#" class="social-ytb"><font-awesome-icon :icon="['fab', 'youtube']" /></a>
             </div>
-    
-            <div class="user-sign">
-              <a href="#" @click="storeGetUser.toggleDisplaySignIn" class="user-action">SignIn</a>
-              <SignIn v-if="storeGetUser.displaySignIn"/>
+            <!-- NATLIB -->
+            <div class="natlib">
+              <img src="@/assets/img/NATLIB_logo.png" alt="the natlib logo" style="width: 35px">
+            </div>
+            <!-- USER -->
+            <div class="user-actions">
+              <div class="user-log">
+                <!-- <span class="user-action">LogIn</span> -->
+                <!-- <div v-show="!storeGetUser.isLogged" @click="storeGetUser.getRegisteredUser" class="user-action">LogIn</div> --> <!-- DELETE?? -->
+                <a href="#" v-show="!storeGetUser.isLogged" @click="storeGetUser.toggleDisplayLogIn" class="user-action">LogIn</a>
+                <a href="#" v-show="storeGetUser.isLogged" @click="storeGetUser.logOutUser" class="user-action">LogOut</a>
+                <LogIn v-if="storeGetUser.displayLogIn"/>
+                <!-- <div v-show="storeGetUser.isLogged" > User: {{ storeGetUser.user.firstName }} {{ storeGetUser.user.lastName }}</div> -->
+              </div>
+              <div class="user-sign">
+                <a href="#" @click="storeGetUser.toggleDisplaySignIn" class="user-action">SignIn</a>
+                <SignIn v-if="storeGetUser.displaySignIn"/>
+              </div>
             </div>
           </div>
         </div>
