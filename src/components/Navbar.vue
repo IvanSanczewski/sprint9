ec9f9a7e1e0cd82f7241893cd64c2df7a69bcf83
IvanSanczewski <isanczewski@gmail.com>
2023-02-13 09:28:38 +0200

    Responsive navbar menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,15 @@
     <RouterLink to="Books"><font-awesome-icon icon="fa-solid fa-book" /></RouterLink>
     <RouterLink to="Movies"><font-awesome-icon icon="fa-solid fa-film" /></RouterLink>
     <RouterLink to="User"><font-awesome-icon icon="fa-solid fa-user" /></RouterLink>
-    <span @click="storeRules.toggleDisplayRules"><font-awesome-icon icon="fa-solid fa-list-check"/></span>
+    <a href="" @click="storeRules.toggleDisplayRules"><font-awesome-icon icon="fa-solid fa-list-check"/></a>
+    <Rules v-if="storeRules.displayRules" />
+  </div>
+  <div class="navbar-container-desk">
+    <RouterLink to="/"> Home</RouterLink>
+    <RouterLink to="Books"> Books</RouterLink>
+    <RouterLink to="Movies"> Movies</RouterLink>
+    <RouterLink to="User"> User</RouterLink>
+    <a href="" @click="storeRules.toggleDisplayRules">Rules</a>
     <Rules v-if="storeRules.displayRules" />
   </div>
 </template>
